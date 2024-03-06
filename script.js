b2a96a33d4b94a5cb6c0d1dc09af5a3b86466751
Kilian Penkner <kilian.penkner@htl-wels.at>
2024-03-06 14:49:17 +0100

    Frontend Page Projects

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore, collection, getDoc, getDocs, addDoc, deleteDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZJTH0Znyi13etPM6Ag5M-lQ_WeqXOIsU",
+    authDomain: "scrumflow-6e479.firebaseapp.com",
+    projectId: "scrumflow-6e479",
+    storageBucket: "scrumflow-6e479.appspot.com",
+    messagingSenderId: "828323679259",
+    appId: "1:828323679259:web:6db3cfbf89942cc3d4fcbe",
+    measurementId: "G-2427QNHC73"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+//let currentProject = null;
+
+// Authentifizierungsstatus beibehalten
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("User is signed in with UID:", user.uid);
+    } else {
+      console.log("No user is signed in.");
+    }
+});
+
 // SIDE MENU
 
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
@@ -60,3 +89,4 @@ switchMode.addEventListener('change', function () {
 		document.body.classList.remove('dark');
 	}
 })
+
