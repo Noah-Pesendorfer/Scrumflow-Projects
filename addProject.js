a03abd30036d3814d8eb8bdd57e02b083a10d297
Kilian Penkner <kilian.penkner@htl-wels.at>
2024-04-03 15:44:21 +0200

    seperate JS

--- addProject.js
+++ addProject.js
@@ -1,3 +1,17 @@
 function addProjectToFirestore1(newProject) {
-    console.log("HALLO");
+    const user = auth.currentUser;
+    if (!user) {
+        alert("You must be logged in to add events.");
+        return;
+    }
+
+    const projectsRef = collection(db, "users", user.uid, "projects");
+    addDoc(projectsRef, newProject).then(docRef => {
+        newProject.id = docRef.id;
+        projectsArr.push(newProject);
+        showProjects();
+        closeIcon.click();
+    }).catch(error => {
+        console.error("Error adding event: ", error);
+    });
 }
