3c95add97918e61a38307d99ef342613b8d660e8
KilianPenk <kilian.penkner@htl-wels.at>
2024-04-06 16:27:33 +0200

    adding Projects

--- addProject.js
+++ addProject.js
@@ -1,21 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getFirestore, collection, getDoc, getDocs, addDoc, deleteDoc, updateDoc, doc, getCountFromServer } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDZJTH0Znyi13etPM6Ag5M-lQ_WeqXOIsU",
-    authDomain: "scrumflow-6e479.firebaseapp.com",
-    projectId: "scrumflow-6e479",
-    storageBucket: "scrumflow-6e479.appspot.com",
-    messagingSenderId: "828323679259",
-    appId: "1:828323679259:web:6db3cfbf89942cc3d4fcbe",
-    measurementId: "G-2427QNHC73"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
 function addProjectToFirestore1(newProject) {
     const user = auth.currentUser;
     if (!user) {
@@ -29,6 +11,6 @@ function addProjectToFirestore1(newProject) {
         showProjects();
         closeIcon.click();
     }).catch(error => {
-        console.error("Error adding event: ", error);
+        console.error("Error adding Project: ", error);
     });
 }
